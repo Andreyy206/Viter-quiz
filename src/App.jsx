5f559e6faa5184a4725f875c19fdd33029b00baa
Andreyy206 <mysibayaneduardandreimaruquez@gmail.com>
2024-11-13 15:02:37 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Quiz from "./components/Quiz";
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route index element={<Welcome />} />
+        <Route path='/quiz' element={<Quiz />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
